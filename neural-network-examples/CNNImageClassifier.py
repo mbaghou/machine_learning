2785c8a5077021bff403537f030d85e412c4adee
mbaghou <mbaghou@ipb.fr>
2019-03-05 00:33:11 +0100

    Image classifier using convolutional neural network

---
+++ neural-network-examples/CNNImageClassifier.py
@@ -0,0 +1,89 @@
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from keras.preprocessing import image
+
+
+class CNNImageClassifier:
+
+    def __init__(self):
+        # Init Sequentiel CNN Model
+        self.classifier = Sequential()
+
+
+    def convolutionAndReLU(self):
+        # Define Convolution + Relu Step : 32 filters of 3x3, input image 64x64 RGB, apply ReLU method
+        self.classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+        print('Init convolution ...')
+
+    def pooling(self):
+        # Max pool with 2x2 filters
+        self.classifier.add(MaxPooling2D(pool_size=(2, 2)))
+        print('Init pooling ...')
+
+    def flat(self):
+        # Convert pooled image as 2-D array to one dimensional single vector
+        self.classifier.add(Flatten())
+        print('Init flatting ...')
+
+    def fullyConnectedLayer(self):
+        self.classifier.add(Dense(units=128, activation='relu'))
+        self.classifier.add(Dense(units=1, activation='sigmoid'))
+        print('Init Fully connected Layers ...')
+
+
+    def compile(self):
+        self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        print('Compiling ...')
+
+    def preprocess(self,training_path, test_path):
+        train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                                shear_range=0.2,
+                                                zoom_range=0.2,
+                                                horizontal_flip=True)
+        test_datagen = ImageDataGenerator(rescale=1. / 255)
+        training_set = train_datagen.flow_from_directory(training_path,
+                                                                   target_size=(64, 64),
+                                                                   batch_size=32,
+                                                                   class_mode='binary')
+        test_set = test_datagen.flow_from_directory(test_path,
+                                                              target_size=(64, 64),
+                                                              batch_size=32,
+                                                              class_mode='binary')
+        self.classifier.fit_generator(training_set,
+                                      steps_per_epoch=1593,
+                                      epochs=25,
+                                      validation_data=test_set,
+                                      validation_steps=2000)
+        print('Prepocessing images ...')
+
+    def build(self,training_path, test_path):
+        self.convolutionAndReLU()
+        self.pooling()
+        self.flat()
+        self.fullyConnectedLayer()
+        self.compile()
+        self.preprocess(training_path, test_path)
+
+    def prediction(self, image_path):
+        print('Start prediction ...')
+        test_image = image.load_img(image_path, target_size=(64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = self.classifier.predict(test_image)
+        self.training_set.class_indices
+        if result[0][0] == 1:
+            prediction = 'dog'
+        else:
+            prediction = 'cat'
+        print('IA predict that the image contain a ' + prediction)
+
+
+if __name__ == '__main__':
+    classifier = CNNImageClassifier()
+    classifier.build('../data/imageclassifier/training_set', '../data/imageclassifier/test_set')
+    classifier.prediction('../data/imageclassifier/test_set/cats/cat.4023.jpg')
--- neural-network-examples/Readme.md
+++ neural-network-examples/Readme.md
@@ -8,3 +8,18 @@ outputNeuronOperation() :  defines the work of the output neuron. It takes 3 par
 perceptron() : calculate the error, used to modify the weights of every connections to the output neuron.
 
 learn() : We create a loop that makes the neural network repeat every situation several times. This part is the learning phase. The number of iteration is chosen according to the precision we want. 
+
+## Simple Image Classification using Convolutional Neural Network — Deep Learning
+We will be building a convolutional neural network that will be trained on few thousand images of cats and dogs, and later be able to predict if the given image is of a cat or a dog.
+
+The process of building a Convolutional Neural Network always involves four major steps.
+
+Step - 1 : Convolution
+
+Step - 2 : Pooling
+
+Step - 3 : Flattening
+
+Step - 4 : Full connection
+
+train/test dataset : https://drive.google.com/drive/folders/1XaFM8BJFligrqeQdE-_5Id0V_SubJAZe?usp=sharing
