ba369d5a81af421f2d39ec4c3eee583f264129bf
mbaghou <mbaghou@ipb.fr>
2019-03-10 19:03:35 +0100

    random forest

--- decision-tree-examples/temperature-degree/TemperaturePredictionRF.py
+++ decision-tree-examples/temperature-degree/TemperaturePredictionRF.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+
 ##https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
 
 class TemperaturePredictionRF:
@@ -17,11 +18,12 @@ class TemperaturePredictionRF:
         self.features = np.array(features)
 
     def train(self):
-        train_ft, test_ft, train_l, test_l = train_test_split(self.features,self.labels, test_size=0.25, random_state=42)
-        rf = RandomForestRegressor(n_estimators=150,random_state=42)
+        train_ft, test_ft, train_l, test_l = train_test_split(self.features, self.labels, test_size=0.25,
+                                                              random_state=42)
+        rf = RandomForestRegressor(n_estimators=150, random_state=42)
         rf.fit(train_ft, train_l)
         predictions = rf.predict(test_ft)
-        print('Mean abs error : ', mean_absolute_error(test_l, predictions) , ' degrees')
+        print('Mean abs error : ', mean_absolute_error(test_l, predictions), ' degrees')
 
 
 if __name__ == '__main__':
